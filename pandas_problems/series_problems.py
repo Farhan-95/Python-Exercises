c3ecd711f75a4d98cd338fd78bd1b24fb838ed7c
Farhan-95 <muhammadfarhankareem95@gmail.com>
2026-07-21 15:05:40 +0500

    pandas series day 1

--- pandas_problems/series_problems.py
+++ pandas_problems/series_problems.py
@@ -1,6 +1,7 @@
 # ========================= Series Problems =======================
 
 import pandas as pd
+import numpy as np
 
 # creating a series from list ----------------
 
@@ -16,4 +17,92 @@ print(mixed_series['num2'])
 
 
 
-# 
+# Series to List Conversion ------------------
+
+series_num = pd.Series([10,40,70,30,20])
+print(type(series_num))
+list_num = list(series_num)
+print(type(list_num))
+print(type(series_num.tolist()))
+
+
+# Series Arithmetic ---------------------
+
+series1 = pd.Series([2, 4, 6, 8, 10])
+series2 = pd.Series([1, 3, 5, 7, 9])
+
+print(type(series1))
+print(type(series2))
+
+add = series1 + series2
+print(add)
+
+sub = series1 - series2
+print(sub)
+
+mul = series1 * series2
+print(mul)
+
+div = series1 / series2
+print(div)
+
+
+
+#  Series Comparison ----------------
+
+series1 = pd.Series([2, 4, 6, 8, 10])
+series2 = pd.Series([1, 3, 5, 7, 10])
+
+print('Equall')
+print(series1 == series2)
+
+print('Greater than')
+print(series1 > series2)
+
+print('Less than')
+print(series1 < series2)
+
+print('Not equall')
+print(series1 != series2)
+
+
+
+# Dict to Series --------------------
+
+dict = {'a': 100, 'b': 200, 'c': 300, 'd': 400, 'e': 800}
+series = pd.Series(dict , name = 'Dict 2 Series')
+print(series)
+
+
+#  Array to Series --------------------
+
+list = [10,20,30,40,50]
+array = np.array(list)
+print(type(array))
+series = pd.Series(array)
+print(type(series))
+
+
+# Change Series DataType -------------------
+
+list = [100 , 200 ,300.12 ,400]
+series = pd.Series(list,name = 'float_converted',dtype = 'float')
+print(series)
+
+
+# DF Column to Series ---------------- 
+
+d = {'col1': [1, 2, 3, 4, 7, 11], 'col2': [4, 5, 6, 9, 5, 0], 'col3': [7, 5, 8, 12, 1, 11]}
+df = pd.DataFrame(data=d)
+
+series = pd.Series(df['col1'])
+print(series)
+
+
+# Series to Array -----------------
+
+list = ['100','200','python','300.12','400']
+series = pd.Series(list)
+array = np.array(series)
+print(type(array))
+print(array)
